8be409068c58dc1bd43c47ce8423652e8e386ec8
Christopher Vigliotti <christopher@christophervigliotti.com>
2022-02-07 08:10:16 -0500

    wip, kinda stuck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,42 @@
+// PARENT COMPONENT
+
 import { Component } from '@angular/core';
+import { ViewChild, AfterViewInit, OnChanges } from '@angular/core'; // 4.A.2 Pass data from Child Component to Parent Component
+import { Child1Component } from './child1/child1.component'; // import the child component, 4.A.2 Pass data from Child Component to Parent Component
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+  implements 
+    AfterViewInit, // 4.A.2 Pass data from Child Component to Parent Component
+    OnChanges
+{
+
+  // directives
+  /* ViewChild() is a property decorator that updates the property if a DOM view change occurs. 
+  It gives the parent access to all the attributes and functions of the child component. */
+  @ViewChild(Child1Component, {static: false}) child1: Child1Component; // Use the @ViewChild() directive, 4.A.2 Pass data from Child Component to Parent Component
+  
+  // properties
   application_title:string = 'Assignment Four';
   application_subtitle:string = "...it's a game.";
+  currentMsgToChild1 = ''; // 4.A.1 Pass data from Parent Component to Child Component
+  msgFromChild1: any; // Declare a variable that holds the data, 4.A.2 Pass data from Child Component to Parent Component
 
-  // 4.A.1 Pass data from Parent Component to Child Component
-  currentMsgToChild1 = '';
+  // methods
+
+  // methods, lifecycle hooks 
+
   ngAfterViewInit(){
-
+    this.msgFromChild1 = this.child1.msgFromChild1; // call the variable that holds the data, 4.A.2 Pass data from Child Component to Parent Component
+    console.log('app ngAfterViewInit, this.msgFromChild1 is ' + this.msgFromChild1);
   }
+
+  ngOnChanges(){
+    console.log('app ngOnChanges()');
+  }
+
 }
--- src/app/child1/child1.component.ts
+++ src/app/child1/child1.component.ts
@@ -1,3 +1,4 @@
+// CHILD COMPONENT
 import { Component, Input, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component'; 
 
@@ -8,9 +9,26 @@ import { AppComponent } from '../app.component';
 })
 export class Child1Component implements OnInit {
 
-  @Input() msgFromParent1: any[];
+  // directives
+  @Input() msgFromParent1: any[]; // 4.A.1 Pass data from Parent Component to Child Component
 
+  // properties
+  // Create two variables, one that holds the data via double-binding and one that is an array, 4.A.2 Pass data from Child Component to Parent Component
+  currentMsgToParent = ''; 
+  msgFromChild1 = [];
+
+  // methods
+
+  // the function that pushes the value of the variable into the array, 4.A.2
+  msgToParent() {  
+    console.log('child1 msgToParent, this.currentMsgToParent is ' + this.currentMsgToParent);
+    this.msgFromChild1.push(this.currentMsgToParent); 
+  }
+
+  // methods, constructor
   constructor() { }
+
+  // methods, lifecycle hooks
 
   ngOnInit(): void {
   }
